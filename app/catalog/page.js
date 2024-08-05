70d99736acdc7fdc162ccb328dc468e31f033f5e
Ken Irwin <kirwin@wittenberg.edu>
2024-08-05 16:45:40 -0400

    show catalog on /catalog

---
+++ app/catalog/page.js
@@ -0,0 +1,61 @@
+'use server'
+
+import CatalogList from "../components/CatalogList";
+import CatalogAddForm from "../components/CatalogAddForm";
+
+async function getCatalog() {
+    const res = await fetch('http://localhost:3000/api/catalog')
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+   
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch catalog data')
+    }
+    
+    return res.json() 
+  }
+
+  async function getInventory() {
+    const res = await fetch('http://localhost:3000/api/inventory')
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+   
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch inventory data')
+    }
+    
+    return res.json() 
+  }
+
+  function addCountsToCatalog(catalogItems,inventory) {
+    return catalogItems.map(item => {
+        const varId = item.variations[0].id; 
+        const matches = inventory.filter(inv => inv.catalogObjectId == varId);
+        if (matches.length > 0) {
+          item.variations[0].quantity = parseInt(matches[0].quantity);
+        } else {
+          item.variations[0].quantity = 0;
+        }
+      })
+    }
+
+export default async function Catalog() {
+    // const catalog = await fetch('http://localhost:3000/api/catalog');
+    const catalogItems = await getCatalog();
+    const inventory = await getInventory();
+    const catalogWithInventory = addCountsToCatalog(catalogItems,inventory.counts);
+    return (<>
+    <h1>Product Catalog</h1>
+    <CatalogAddForm></CatalogAddForm>
+    <CatalogList catalogItems={catalogItems} counts={inventory.counts}></CatalogList>
+    <h2>Length { catalogItems.length }</h2>
+    <pre>
+    {JSON.stringify(catalogItems,null,2)}
+    </pre>
+    <pre>
+        {JSON.stringify(inventory.counts, null,2)}
+    </pre>
+    </>)
+}
