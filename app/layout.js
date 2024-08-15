8b1f94a7e26b47a771eaaeb9a89978f48b955ae0
Ken Irwin <kirwin@wittenberg.edu>
2024-08-15 15:38:30 -0400

    add header

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.css'
 import "./globals.css";
+import Header from './components/Header';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className + ' ' +'container'}>{children}</body>
+      <body className={inter.className}>
+        <Header></Header>
+        <main className="container">
+         {children}
+        </main>
+      </body>
     </html>
   );
 }
