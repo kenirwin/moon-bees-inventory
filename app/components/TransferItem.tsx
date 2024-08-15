d774572ba66493ede805fef651fa290203effcb2
Ken Irwin <kirwin@wittenberg.edu>
2024-08-14 21:22:24 -0400

    reads quantities from api, calculates preview

--- app/components/TransferItem.tsx
+++ app/components/TransferItem.tsx
@@ -1,18 +1,21 @@
 interface Props {
     productId: string;
     productName: string;
-    quantity: number;
+    quantity: string;
     toOrFrom: "to" | "from";
     adjustmentQty: number; 
 }
 
 const TransferItem = ({productName, quantity, toOrFrom, adjustmentQty = 0, productId}: Props) => { 
+    console.log('quantity', typeof quantity, quantity)
+    console.log('adj', typeof adjustmentQty, adjustmentQty)
+    const q = parseInt(quantity)
     return <div className="card">
         <h2 className="capitalize">{toOrFrom}:</h2>
         <h3>{productName}</h3>
         <p>Quantity: {quantity}</p>
-        <p>New Qty after transfer: {toOrFrom == "to" ? quantity + adjustmentQty : quantity - adjustmentQty} </p>
-        <input type="hidden" data-productid={productId} data-transferdirection={toOrFrom} data-newvalue={toOrFrom == "to" ? quantity + adjustmentQty : quantity - adjustmentQty}></input>
+        <p>New Qty after transfer: {toOrFrom == "to" ? q + adjustmentQty : q - adjustmentQty} </p>
+        <input type="hidden" data-productid={productId} data-transferdirection={toOrFrom} data-newvalue={toOrFrom == "to" ? q + adjustmentQty : q - adjustmentQty}></input>
     </div>
 }
 
