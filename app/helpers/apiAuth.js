ba5096e8dc5d212bb344f5756f270100b8451bf9
Ken Irwin <kirwin@wittenberg.edu>
2024-08-15 16:12:06 -0400

    handle sandbox vs production in apiAuth

--- app/helpers/apiAuth.js
+++ app/helpers/apiAuth.js
@@ -1,10 +1,11 @@
 const { Client, Environment, ApiError } = require('square');
 
+const squareEnv = (process.env.VERCEL_ENV == 'production') ? Environment.Production : Environment.Sandbox
 const client = new Client({
     bearerAuthCredentials: {
       accessToken: process.env.ACCESS_TOKEN
     },
-    environment: Environment.Sandbox
+    environment: squareEnv
 });
 
 export default client;
