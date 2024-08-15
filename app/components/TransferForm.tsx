75c9ea1166c8123a439f6a96057b94900c9fceb8
Ken Irwin <kirwin@wittenberg.edu>
2024-08-14 21:32:30 -0400

    submitting transfer works! not reflected on page

--- app/components/TransferForm.tsx
+++ app/components/TransferForm.tsx
@@ -56,6 +56,15 @@ const TransferForm = ({}: Props) => {
         }
         
         console.log(queryObject);
+
+        fetch('http://localhost:3000/api/inventory/transfer', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(queryObject),
+            }).then(response => response.json())
+            .then(data => console.log('updated inventory:',data))
     }
 
     return (
