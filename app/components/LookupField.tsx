d61a3221210b75bd40eca182bb1742d9957f1597
Ken Irwin <ken@kenirwin.net>
2024-08-16 16:50:50 -0400

    only lookup with at least 4 characters

--- app/components/LookupField.tsx
+++ app/components/LookupField.tsx
@@ -1,27 +1,34 @@
-'use client'
-import {useState} from 'react'
+'use client';
+import { useState } from 'react';
 
-interface Props { 
-  setResults: Function,
-  setResultsVisibility: Function
+interface Props {
+  setResults: Function;
+  setResultsVisibility: Function;
 }
-const LookupField = ({setResults, setResultsVisibility}: Props) => {
+const LookupField = ({ setResults, setResultsVisibility }: Props) => {
   const [lookupValue, setLookupValue] = useState('');
-    
+
   const handleLookupChange = async (value) => {
+    setLookupValue(value);
+    if (value.length > 3) {
       setResultsVisibility(true);
-      setLookupValue(value);
       let res = await fetch(`/api/catalog/search?q=${value}`);
       let json = await res.json();
-      setResults(json)
+      setResults(json);
       console.log('Lookup Results:', json);
-  }
+    }
+  };
 
   return (
     <div className="lookup-form">
-        <input type="text" value={lookupValue} onChange={(e => handleLookupChange(e.target.value))} placeholder='Item Lookup'></input>
+      <input
+        type="text"
+        value={lookupValue}
+        onChange={(e) => handleLookupChange(e.target.value)}
+        placeholder="Item Lookup"
+      ></input>
     </div>
-  )
-}
+  );
+};
 
-export default LookupField
+export default LookupField;
